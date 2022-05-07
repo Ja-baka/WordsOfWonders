5ace1a7bc3a1a8130c5d66bfe0664c14f2a70788
Ja-baka <73071447+Ja-baka@users.noreply.github.com>
2022-05-07 18:45:03 +0300

    add Line Drawer

--- Assets/WordsOfWonder/Scripts/LineDrawer.cs
+++ Assets/WordsOfWonder/Scripts/LineDrawer.cs
@@ -1,55 +1,65 @@
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class LineDrawer : MonoBehaviour
 {
-    private LineRenderer _lineRenderer;
-    public void Start()
-    {
-        _lineRenderer = gameObject.AddComponent<LineRenderer>();
-        _lineRenderer.SetWidth(0.2f, 0.2f);
-        _lineRenderer.enabled = false;
-    }
-
     private Vector3 _initialPosition;
     private Vector3 _currentPosition;
+    private LineRenderer _lineRenderer;
+
+    public void Start()
+    {
+        _lineRenderer = gameObject.GetComponent<LineRenderer>();
+
+        _lineRenderer.startWidth = 0.1f; 
+        _lineRenderer.endWidth = 0.1f; 
+    }
+
     public void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            _initialPosition = GetCurrentMousePosition().GetValueOrDefault();
-            _lineRenderer.SetPosition(0, _initialPosition);
-            _lineRenderer.SetVertexCount(1);
-            _lineRenderer.enabled = true;
+            MouseDown();
         }
         else if (Input.GetMouseButton(0))
         {
-            _currentPosition = GetCurrentMousePosition().GetValueOrDefault();
-            _lineRenderer.SetVertexCount(2);
-            _lineRenderer.SetPosition(1, _currentPosition);
+            MouseHold();
 
         }
         else if (Input.GetMouseButtonUp(0))
         {
-            _lineRenderer.enabled = false;
-            var releasePosition = GetCurrentMousePosition().GetValueOrDefault();
-            var direction = releasePosition - _initialPosition;
-            Debug.Log("Process direction " + direction);
+            MouseUp();
         }
     }
 
-    private Vector3? GetCurrentMousePosition()
+    private void MouseDown()
     {
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        var plane = new Plane(Vector3.forward, Vector3.zero);
+        _initialPosition = GetCurrentMousePosition();
+        _lineRenderer.SetPosition(0, _initialPosition);
+        _lineRenderer.positionCount = 1;
 
-        float rayDistance;
-        if (plane.Raycast(ray, out rayDistance))
-        {
-            return ray.GetPoint(rayDistance);
-
-        }
-
-        return null;
+        _lineRenderer.enabled = true;
     }
 
+    private void MouseHold()
+    {
+        _currentPosition = GetCurrentMousePosition();
+
+        _lineRenderer.positionCount = 2;
+        _lineRenderer.SetPosition(1, _currentPosition);
+    }
+
+    private void MouseUp()
+    {
+        _lineRenderer.enabled = false;
+    }
+    private Vector3 GetCurrentMousePosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.forward, Vector3.zero);
+
+        return plane.Raycast(ray, out float rayDistance) 
+            ? ray.GetPoint(rayDistance) 
+            : Vector3.zero;
+    }
 }
